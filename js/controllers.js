9ab6fe6e19a50eaab996420a80a20f6477ca4b85
shahzeb1 <shahzeb001@gmail.com>
2015-10-24 17:16:04 -0400

    All the changes

--- js/controllers.js
+++ js/controllers.js
@@ -20,9 +20,16 @@ flippedCtrl.controller('signupCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
     $scope.signupType = $routeParams.type;
     // Check if the type is student or teacher
-    if($scope.signupType != "student" || $scope.signupType != "teacher"){
+    if($scope.signupType != "student" && $scope.signupType != "teacher"){
     	$scope.signupType = "student";	
     }
+    $scope.user = {};
+    // Signup button clicked
+    $scope.signup = function(){
+
+      alert('you called me');
+    }
+
   }]);
 
 flippedCtrl.controller('loginCtrl', ['$scope', '$routeParams',
