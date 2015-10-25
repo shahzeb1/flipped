5010dd6ac41f114488870eab995b0099e264d31e
shahzeb1 <shahzeb001@gmail.com>
2015-10-24 20:36:24 -0700

    Removed the use of routes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
+app.use('/', express.static(path.join(__dirname, 'public')));
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
