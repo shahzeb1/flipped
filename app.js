bd86abdfe6c1bfc45a2e44239483eb79567a4942
shahzeb1 <shahzeb001@gmail.com>
2015-10-24 20:38:52 -0700

    Removed the use of routes

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img/icon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
